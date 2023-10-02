103ac0399b3c3284cce5f650c8ae16b60538ba01
BrokeApeCronosClub <145580513+BrokeApeCronosClub@users.noreply.github.com>
2023-10-02 11:38:42 -0400

    Update IframeComponent.js

--- app/IframeComponent.js
+++ app/IframeComponent.js
@@ -5,7 +5,8 @@ const IframeComponent = () => {
     overflow: 'visible',
     border: '5px solid rgba(0, 0, 0, 0.6)', // 60% transparent black border
     borderRadius: '10px', // Rounded corners with a 15px radius
-     marginRight: '10px', // Add a 20px right margin
+    marginRight: '10px', // Add a 20px right margin
+  };
 
   return (
     <iframe
@@ -14,7 +15,6 @@ const IframeComponent = () => {
       height="615px"
       style={iframeStyle} // Apply the style here
       frameBorder="0"
-      
     ></iframe>
   );
 };
